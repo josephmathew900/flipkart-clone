2d818a83c8cace7b48e2bb704d31737d19fbe95a
josephmathew900 <josephmathew900@gmail.com>
2021-05-16 00:49:07 +0530

    backend signup done

--- ecommerce-back-end/src/index.server.js
+++ ecommerce-back-end/src/index.server.js
@@ -1,20 +1,24 @@
 const env = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
 
 env.config();
 
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'Hello from server' });
-});
-
-app.post('/data', (req, res, next) => {
-  res.status(200).json({
-    message: req.body.data,
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8otsi.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+  )
+  .then(() => {
+    console.log('Database connected');
   });
-});
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
