5efa6041742d3097442f9a97e3b906ac160f6438
josephmathew900 <josephmathew900@gmail.com>
2021-05-16 11:06:21 +0530

    admin app layout - signin & signup

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import { Jumbotron } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import Signin from './containers/Signin';
+import Signup from './containers/Signup';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Jumbotron>
-        <h1>Admin Dashboard</h1>
-      </Jumbotron>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
