412f06a15100d9271a34a9667be5981525f490e0
josephmathew900 <josephmathew900@gmail.com>
2021-05-16 18:40:25 +0530

    product creation api

--- ecommerce-back-end/src/models/category.js
+++ ecommerce-back-end/src/models/category.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true },
-    slug: { type: String, required: true, unique: true },
-    parentId: { type: String },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    parentId: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
